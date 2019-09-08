3bae207b7b010c7f178bb63f142667c3a07d7296
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-08 19:31:58 -0400

    Completed the map function to create the list of people record information

--- src/components/PeopleRecords.js
+++ src/components/PeopleRecords.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const PeopleRecords = props => {
-    const peopleRecordsList= props.records.map();
+    const peopleRecordsList = props.people.map((person) => {
+        return(
+            <div className="card" key={person.id}>
+                <div className="content">
+                <div className="header">{person.name}</div>
+                <div className="meta">{person.jobtitle}</div>
+                <div className="ui list-group">
+                    <h3 className="ui list-group-item">{person.email}</h3>
+                    <h3 className="ui list-group-item">{person.address}</h3>
+                </div>
+                </div> 
+            </div>
+        ) 
+    });
     return (
         <div>
             
