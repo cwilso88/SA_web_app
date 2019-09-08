954f33694b1b57561df4ca52eae963e5ddfa3365
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-08 18:25:28 -0400

    Added styling to Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './peopleRecords.css';
+import Header from './components/Header.js';
+import PeopleRecords from './components/PeopleRecords.js';
+import CharCountButton from './components/CharCountButton.js';
 
 function App() {
   return (
     <div className="App">
-      SalesLoft Web App
+      <Header />
+      <PeopleRecords />
+      <CharCountButton />
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const header = () => {
+
+const Header = () => {
     return (
-        <div>
-           Header 
+        <div className="ui container">
+            <h1 className="ui header blue">People Records</h1>
+            <h5 className="ui dividing header">The place to locate information on a person in a cadence.</h5>
         </div>
     )
 }
 
-export default header;
+export default Header;
