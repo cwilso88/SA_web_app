922cf816a43a34bfc6f3abef11262a6a79419742
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-08 21:06:12 -0400

    Changed App.js to a class based component to use lifecycle funcions

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import PeopleRecords from './components/PeopleRecords.js';
 import CharCountButton from './components/CharCountButton.js';
 import Menu from './components/Menu';
 
-function App() {
-  return (
-    <div className="App">
-      <Menu />
-      <Header />
-      <PeopleRecords />
-      <CharCountButton />
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+            <Menu />
+            <Header />
+            <PeopleRecords />
+            <CharCountButton />
+        </div>
+      )
+    }
 }
 
 export default App;
