74c53adeaa1a8459b83389f103fa3c6a5d160528
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-08 20:16:32 -0400

    Set up the api.js component with axios

--- src/components/PeopleRecords.js
+++ src/components/PeopleRecords.js
@@ -5,11 +5,11 @@ const PeopleRecords = props => {
         return(
             <div className="card" key={person.id}>
                 <div className="content">
-                <div className="header">{person.name}</div>
-                <div className="meta">{person.jobtitle}</div>
+                <div className="header">{person.first_name} {person.last_name}</div>
+                <div className="meta">{person.title}</div>
                 <div className="ui list-group">
-                    <h3 className="ui list-group-item">{person.email}</h3>
-                    <h3 className="ui list-group-item">{person.address}</h3>
+                    <h3 className="ui list-group-item">{person.email_address}</h3>
+                    <h3 className="ui list-group-item">{person.city}, {person.state}</h3>
                 </div>
                 </div> 
             </div>
